f8590222306e757e5e1a2e4a0ffc2a95b142b5e9
iftikhar <ifti891@gmail.com>
2022-04-04 14:13:07 +0530

    add dayOne folder

---
+++ dayOne/newAppOne.js
@@ -0,0 +1,19 @@
+console.log("my My");
+
+let x = 5;
+let y = 6;
+let z = x + y;
+
+let x = 5;
+let y = 6;
+let z = x + y;
+
+let x = 5;
+let y = 6;
+let z = x + y;
+
+let x = 5;
+let y = 6;
+let z = x + y;
+
+alert("Made some changes")
