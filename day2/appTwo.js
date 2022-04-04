624734637280331de6cd2a399101bee6c334c21b
iftikhar <ifti891@gmail.com>
2022-04-04 11:47:39 +0530

    another chnage

--- day2/appTwo.js
+++ day2/appTwo.js
@@ -1,1 +1,5 @@
 console.log("my My");
+
+let x = 5;
+let y = 6;
+let z = x + y;
---
+++ day3/app3.js
@@ -0,0 +1,10 @@
+alert("Hello not world!");
+console.log("Made some changes")
+
+let x = 5;
+let y = 6;
+let z = x + y;
+
+let x = 5;
+let y = 6;
+let z = x + y;
